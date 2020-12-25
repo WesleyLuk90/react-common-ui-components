4b5695760af38a404088babecf12b24021a001e8
Wesley Luk <wesleyluk@gmail.com>
2020-12-25 12:54:21 -0800

    WIP

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,3 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    entry: "./src/index.ts",
+    mode: "development",
+    entry: "./docs/src/index.ts",
+    output: {
+        path: __dirname + "/dist",
+    },
+    devServer: {
+        contentBase: __dirname + "/dist",
+    },
+    devtool: "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "React Common UI Components API Documentation",
+        }),
+    ],
 };
